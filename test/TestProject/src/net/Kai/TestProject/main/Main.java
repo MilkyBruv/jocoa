e0626003a915b33ca855a5e961c41e0f82b952bb
Kai <71107159+MilkyBruv@users.noreply.github.com>
2025-05-21 18:40:30 +1200

    actuals v0.1.1

---
+++ test/TestProject/src/net/Kai/TestProject/main/Main.java
@@ -0,0 +1,14 @@
+package net.Kai.TestProject.main;
+
+import net.Kai.TestProject.test.Test;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+		Test.say("How are you?");
+
+	}
+
+}
---
+++ test/TestProject/src/net/Kai/TestProject/test/Test.java
@@ -0,0 +1,11 @@
+package net.Kai.TestProject.test;
+
+public class Test {
+    
+    public static final void say(String message) {
+
+        System.out.println("Message: " + message);
+
+    }
+
+}
